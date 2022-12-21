e3ea5d60dc8ee2af4e07f1a106ea688830a4f6f6
Jan Dostal <jan.dostal@tul.cz>
2022-12-21 21:13:56 +0100

    Included only required files

--- SemestralProject/dist/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"cz.tul.fm.alg1.dostal","l":"ChristmasApplesDrawing"},{"p":"cz.tul.fm.alg1.dostal","l":"MaxScalarProductCalculation"},{"p":"cz.tul.fm.alg1.dostal","l":"UI"}];updateSearchResults();
